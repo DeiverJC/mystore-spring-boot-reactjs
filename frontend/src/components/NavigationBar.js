43133358535a28ffec07b45069e25bfd068744fe
DeiverJC <deiver-jc@live.com>
2023-03-25 18:15:22 -0500

    update navigation component

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,13 +1,12 @@
-import { AppBar, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 
 export default function NavigationBar() {
   return (
-    <AppBar position="static" color="secondary">
+    <AppBar position="absolute" color="secondary">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           MyStore
         </Typography>
-        <Button color="inherit">Add Product</Button>
       </Toolbar>
     </AppBar>
   );
