2ed1f7e73577274868a5a429f26504ef07a1dd15
DeiverJC <deiver-jc@live.com>
2023-03-25 02:46:49 -0500

    add cors to ProductController

--- backend/src/main/java/dev/deiverjc/backend/Controllers/ProductController.java
+++ backend/src/main/java/dev/deiverjc/backend/Controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import dev.deiverjc.backend.Entities.Product;
 import dev.deiverjc.backend.Services.ProductServiceImpl.PSImpl;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("products")
 public class ProductController {
 
