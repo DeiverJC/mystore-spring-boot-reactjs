e0cb61149d69d29dbb4a58b607b0ac27a053c9f1
DeiverJC <deiver-jc@live.com>
2023-03-25 18:15:08 -0500

    update layout with MUI

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -7,7 +7,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Container } from "@mui/material";
+import { Box, Container, Toolbar } from "@mui/material";
 
 export const metadata = {
   title: "MyStore App",
@@ -18,10 +18,25 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body style={{ margin: 0 }}>
-        <NavigationBar />
-        <Container maxWidth="md">
-          <ProductContextProvider>{children}</ProductContextProvider>
-        </Container>
+        <ProductContextProvider>
+          <Box sx={{ display: "flex" }}>
+            <NavigationBar />
+            <Box
+              component="main"
+              sx={{
+                backgroundColor: (theme) => theme.palette.grey[100],
+                flexGrow: 1,
+                height: "100vh",
+                overflow: "auto",
+              }}
+            >
+              <Toolbar />
+              <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                {children}
+              </Container>
+            </Box>
+          </Box>
+        </ProductContextProvider>
       </body>
     </html>
   );
